969e5946a49917f6a75b6d526f1e44094cda518a
Steven <stevenlin3263@gmail.com>
2023-03-14 14:35:38 +0800

    create the different environment through webpack

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <>
-      <h1>This is App file</h1>
+      <h1>This is App file {process.env.NODE_ENV} {process.env.name}</h1>
       <img src={require('./assets/image/test.jpeg')} alt="test" />
     </>
   )
--- client/webpack/webpack.config.js
+++ client/webpack/webpack.config.js
@@ -1,38 +1,10 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const commonConfig = require('./webpack.common.js')
+const { merge } = require('webpack-merge')
 
-module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      }, {
-        test: /\.(s(a|c)ss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }, {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource'
-      }
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-  ]
+module.exports = (envVars) => {
+  const { env } = envVars
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, envConfig)
+
+  return config
 }
