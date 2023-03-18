9ac5cd18bd06b6401c5b7ae9b11b1245ef3b4ee1
Steven <stevenlin3263@gmail.com>
2023-03-18 13:15:02 +0800

    [feat] complete the project page layout

--- client/src/app/common/layouts/MainFeatureLayout/MainFeatureLayout.tsx
+++ client/src/app/common/layouts/MainFeatureLayout/MainFeatureLayout.tsx
@@ -4,7 +4,7 @@ import MainContent from '../MainContent';
 const MainFeatureLayout: React.FC = (props) => {
   return (
     <>
-      <div className="d-flex main-layout h-100">
+      <div className="h-100 d-flex flex-column justify-content-center align-items-center">
         <MainContent>{props.children}</MainContent>
       </div>
     </>
--- client/src/app/feature/Feature/Project/Project.tsx
+++ client/src/app/feature/Feature/Project/Project.tsx
@@ -1,7 +1,90 @@
 import React from 'react';
 
 const Project: React.FC = () => {
-  return <h1>Projects</h1>;
+  const date = new Date();
+  const today =
+    date.getFullYear() + '-' + `${date.getMonth() + 1}` + '-' + date.getDate();
+  return (
+    <div className="project-container">
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+        <div className="project-card">
+          <p className="d-flex justify-content-end">{today}</p>
+          <p className="text-center font-lg">project name</p>
+          <div className="d-flex justify-content-end">
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+            <div className="avatar">SL</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
